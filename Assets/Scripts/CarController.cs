698533d1febd7e8df11220567419a81d454798fb
Chitvan <chitvan.atpug@gmail.com>
2014-11-08 06:54:55 -0800

    Merged/Rewrote gameplay logic!
    Added the movement of objects for turns

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pose = Thalmic.Myo.Pose; 
+
+public class CarController : MonoBehaviour {
+
+
+	float timer =0f;
+	Vector3 startPos;
+	Quaternion startRot;
+	string moveDirection = "";
+	CarMovement carMoveScript;
+	bool isCarSet = false;
+
+	public float turnSpeed =3f;
+	public GameObject car;
+	public GameObject myo = null;
+
+
+	private Pose _lastPose = Pose.Unknown;
+
+	void Start()
+	{
+		if(car!=null)
+		{
+			carMoveScript = car.GetComponent<CarMovement>();
+		}
+	}
+
+	void OnGUI ()
+	{
+		GUI.skin.label.fontSize = 12;
+		
+		ThalmicHub hub = ThalmicHub.instance;
+		
+		// Access the ThalmicMyo script attached to the Myo object.
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+		
+		if (!hub.hubInitialized) {
+			GUI.Label(new Rect (12, 8, Screen.width, Screen.height),
+			          "Cannot contact Myo Connect. Is Myo Connect running?\n" +
+			          "Press Q to try again."
+			          );
+		} else if (!thalmicMyo.isPaired) {
+			GUI.Label(new Rect (12, 8, Screen.width, Screen.height),
+			          "No Myo currently paired."
+			          );
+		} else if (!thalmicMyo.armRecognized) {
+			GUI.Label(new Rect (12, 8, Screen.width, Screen.height),
+			          "Perform the Sync Gesture."
+			          );
+		} else {
+			GUI.Label (new Rect (12, 8, Screen.width, Screen.height),
+			           "Score: 0"
+			           );
+		}
+	}
+	
+	void Update ()
+	{
+		if(car!=null)
+		{
+			if(carMoveScript.moving == false)
+			{
+				if(!isCarSet)
+				{
+					isCarSet = true;
+					setCars();
+				}
+				DetectInput();
+				MoveCars();
+			}
+		}
+	}
+
+
+	void DetectInput()
+	{
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+		if (thalmicMyo.pose == _lastPose) {
+			return;
+		}
+		_lastPose = thalmicMyo.pose;
+		if (thalmicMyo.pose == Pose.WaveIn) //left
+		{
+			moveDirection = "left";
+		}
+		if (thalmicMyo.pose == Pose.WaveOut) //right
+		{
+			moveDirection = "right"; 
+		}
+		if (thalmicMyo.pose == Pose.FingersSpread) //stop
+		{
+			moveDirection = "stop";
+		}
+		if (thalmicMyo.pose == Pose.Fist) //go straight
+		{
+			moveDirection = "forward";
+		}
+	}
+
+	void MoveCars()
+	{
+		if(moveDirection == "left")
+		{
+			timer+= Time.deltaTime*turnSpeed;
+			if(timer<1f)
+			{
+				car.transform.localPosition = Vector3.Lerp(startPos, startPos+ new Vector3(-5, 0, -3) , timer);
+				car.transform.localRotation = Quaternion.Slerp(startRot, startRot * new Quaternion(0,0.7f,0,0.7f) , timer);
+			}
+			else if(timer>=1f && timer<2f)
+			{
+				transform.localPosition -= new Vector3(0,0, timer);
+			}
+			else if(timer>2f)
+			{
+				moveDirection = "";
+			}
+		}
+		else if(moveDirection == "right")
+		{
+			timer+= Time.deltaTime*turnSpeed;
+			if(timer<1f)
+			{
+				car.transform.localPosition = Vector3.Lerp(startPos, startPos+ new Vector3(5, 0, -3) , timer);
+				car.transform.localRotation = Quaternion.Slerp(startRot, startRot * new Quaternion(0,-0.7f,0,0.7f) , timer);
+			}
+			else if(timer>=1f && timer<2f)
+			{
+				transform.localPosition -= new Vector3(0,0, timer);
+			}
+			else if(timer>2f)
+			{
+				moveDirection = "";
+			}
+		}
+		else if(moveDirection == "stop")
+		{
+			
+		}
+		else if(moveDirection == "forward")
+		{
+			
+		}
+	}
+
+	void setCars()
+	{
+		timer = 0f;
+		moveDirection = "";
+		startPos = car.transform.localPosition;
+		startRot = car.transform.localRotation;
+
+	}
+}
---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour {
+
+	public float speed =3f ;
+
+	//public CarController controller;
+
+	public bool moving = true;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Move();
+	}
+
+	void Move()
+	{
+		if(transform.localPosition.z>0 )
+		{
+			moving = true;
+			transform.localPosition -= new Vector3(0,0, Time.deltaTime*3);
+		}
+		else if(transform.localPosition.z<=0)
+		{
+			//set the controller
+			moving = false;
+		}
+	}
+}
