80f34e32719642edd5e5b837dc19b47981054b4d
Chitvan <chitvang@usc.edu>
2014-11-09 04:02:59 -0800

    Arrow fixes

--- Assets/Scripts/GenerateSign.cs
+++ Assets/Scripts/GenerateSign.cs
@@ -16,25 +16,44 @@ public class GenerateSign : MonoBehaviour {
 	
 		if(!isDirectionSet)
 		{
-			int i = Random.Range(1,1000)%4;
+			int i = Random.Range(1,1000)%3;
 			renderer.material.mainTexture = textures[0];
 
 			switch(i)
 			{
-			case 1:
-				//renderer.material.mainTextureScale = new Vector2(-1, -1);
-				renderer.material.mainTexture = textures[0];
-				carMove.goingTo = 1;
-				break;
-			case 2:
-				//renderer.material.mainTextureScale = new Vector2(1, 1);
-				renderer.material.mainTexture = textures[1];
+			case 0: //left
+				print (transform.parent.name);
+				if(transform.parent.name.Contains("CarRight"))
+				{
+					print (1);
+					renderer.material.mainTextureScale = new Vector2(-1, -1);
+
+				}
+				else
+					renderer.material.mainTexture = textures[0];
 				carMove.goingTo = 0;
 				break;
-			case 3:
+			case 1: //right
+				print(transform.parent.name);
+				if(transform.parent.name.Contains("CarRight"))
+				{
+					renderer.material.mainTextureScale = new Vector2(-1, 1);
+					renderer.material.mainTexture = textures[0];
+				}
+				else
+					renderer.material.mainTexture = textures[1]; 
+				carMove.goingTo = 1;
 
-				carMove.goingTo = 2;
+				break;
+			case 2:
+				if(transform.parent.name.Contains("CarRight"))
+				{
+					renderer.material.mainTextureScale = new Vector2(-1, -1);
+
+				}
 				renderer.material.mainTexture = textures[2];
+				carMove.goingTo = 2;
+
 				break;
 			}
 			isDirectionSet = true;
